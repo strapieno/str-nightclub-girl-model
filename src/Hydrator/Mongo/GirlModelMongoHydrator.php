c059c9ff6d0c667343df60e0f15635fb6250ac9a
visa4 <antonino.visalli@gmail.com>
2016-05-23 14:30:19 +0200

    Fix config

--- src/Hydrator/Mongo/GirlModelMongoHydrator.php
+++ src/Hydrator/Mongo/GirlModelMongoHydrator.php
@@ -2,6 +2,8 @@
 namespace Strapieno\NightClubGirl\Model\Hydrator\Mongo;
 
 use Matryoshka\Model\Hydrator\Strategy\HasOneStrategy;
+use Matryoshka\Model\Hydrator\Strategy\SetTypeStrategy;
+use Strapieno\NightClub\Model\Entity\Object\AggregateRatingObject;
 use Strapieno\NightClub\Model\Entity\Reference\NightClubReference;
 use Strapieno\Utils\Hydrator\Mongo\DateHistoryHydrator;
 
@@ -13,11 +15,17 @@ class GirlModelMongoHydrator extends DateHistoryHydrator
     public function __construct($underscoreSeparatedKeys = true)
     {
         parent::__construct($underscoreSeparatedKeys);
-
+        // Property strategy
         $this->addStrategy(
             'night_club_reference',
             new HasOneStrategy(new NightClubReference(), false)
         );
 
+        $aggregateRating = new AggregateRatingObject();
+        $aggregateRating->getHydrator()->addStrategy('partial', new SetTypeStrategy('array', 'array'));
+        $this->addStrategy(
+            'aggregate_rating',
+            new HasOneStrategy($aggregateRating, false)
+        );
     }
 }
