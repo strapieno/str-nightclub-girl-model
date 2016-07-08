02d97fedf47736d5a76028b98caa8f80ce823c07
visa4 <antonino.visalli@gmail.com>
2016-07-08 16:58:24 +0200

    Fix collection criteria

--- src/Criteria/Mongo/GirlMongoCollectionCriteria.php
+++ src/Criteria/Mongo/GirlMongoCollectionCriteria.php
@@ -8,5 +8,14 @@ use Matryoshka\Model\Wrapper\Mongo\Criteria\FindAllCriteria;
  */
 class GirlMongoCollectionCriteria extends FindAllCriteria
 {
-
+    /***
+     * @param $nightclubId
+     * @return $this
+     */
+    public function setNightclubId($nightclubId)
+    {
+        // TODO add hydrator
+        $this->selectionCriteria['nightclub_id'] = (string) $nightclubId;
+        return $this;
+    }
 }
